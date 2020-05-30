1f7ae4de677d27bdf65c1058ce3f2282ba148c79
Margaret Wismer <margaretgwismer@gmail.com>
2020-05-30 09:42:13 -0400

    added basic routes

--- src/components/front-page/front-page.tsx
+++ src/components/front-page/front-page.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import './front-page.scss';
+import { NavLink } from 'react-router-dom';
 
 export const FrontPage: React.FC = () => {
     const handleCreateNewPearl = useCallback(() => {
@@ -14,8 +15,18 @@ export const FrontPage: React.FC = () => {
         <div>
             <h2> Welcome to Pearl Protect </h2>
             <div className='frontpage-buttons'>
-                <div className='frontpage-buttons__create' onClick={handleCreateNewPearl}> Create New Pearl </div>
-                <div className='frontpage-buttons__add' onClick={handleAddForExistingPearl}> Add for existing Pearl </div>
+            <button className='frontpage-buttons__create'>
+                <NavLink to={'/NewPearl'} style={{color: 'black', textDecoration: 'none', border: '0px'}}>
+                    <div>Create New Pearl</div>
+                </NavLink>
+            </button>
+            <button className='frontpage-buttons__add'>
+                <NavLink to={'/ExistingPearl'} style={{color: 'black', textDecoration: 'none', border: '0px'}}>
+                   Add for existing Pearl
+                </NavLink>
+            </button>
+                {/* <div className='frontpage-buttons__create' onClick={handleCreateNewPearl}> Create New Pearl </div>
+                <div className='frontpage-buttons__add' onClick={handleAddForExistingPearl}> Add for existing Pearl </div> */}
             </div>
         </div>
     )
